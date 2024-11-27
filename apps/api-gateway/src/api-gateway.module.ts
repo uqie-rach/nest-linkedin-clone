1417f3400e29f75412332a8fdda6d3f876f90fdb
Achmad Furqon Rachmadie <achmadfurqonrachmadie@gmail.com>
2024-11-28 00:22:16 +0700

    improve: implemented auth guard on user resource

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -1,16 +1,30 @@
 import { Module } from '@nestjs/common';
 
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 import { ApiGatewayController } from './api-gateway.controller';
 import { UsersController } from './users/users.controller';
 
 import { ApiGatewayService } from './api-gateway.service';
 import { UsersService } from './users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [ApiGatewayController, UsersController],
-  providers: [ApiGatewayService, UsersService],
+  providers: [
+    ApiGatewayService,
+    UsersService,
+    JwtService,
+    ConfigService,
+  ],
 })
 export class ApiGatewayModule {}
--- apps/api-gateway/src/users/users.controller.ts
+++ apps/api-gateway/src/users/users.controller.ts
@@ -3,50 +3,63 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 import { CreateUserDto, FindOneDto, UpdateUserDto } from 'contracts/user.dto';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @HttpCode(HttpStatus.CREATED)
   @Post('auth/register')
   async register(@Body() createUserDto: CreateUserDto) {
     return this.usersService.register(createUserDto);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('auth/login')
   async login(@Body() body: { email: string; password: string }) {
     const { email, password } = body;
     return this.usersService.login(email, password);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
   @Get(':id')
   findOne(@Param() params: FindOneDto) {
-    console.log(params)
     return this.usersService.findOne(params.id);
   }
 
+  @HttpCode(HttpStatus.CREATED)
   @Post()
   create(@Body() user: CreateUserDto) {
     return this.usersService.create(user);
   }
-
+  
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
   @Put(':id')
   update(@Param('id') id: string, @Body() user: UpdateUserDto) {
     return this.usersService.update(id, user);
   }
-
+  
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
   @Delete(':id')
   delete(@Param('id') id: string) {
     return this.usersService.remove(id);
